19be054e422f8b87276792d4f420a72516296d47
vashaferov <113062270+vashaferov@users.noreply.github.com>
2026-08-23 09:44:59 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-const API_URL = 'https://script.google.com/macros/s/AKfycbwEQj7cN6cYUcK4OZsyT6y3CZXclEgYRV4fidIaahCq3lRmTDBdSZez8bDQptQviNo/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbwYWJxHXFjbpkykyVW3g0Tq2V7thHmZJEG5_ygfsYnni2l3suRozsdKL0pLLMD2GPI/exec';
 const PAGE_SIZE = 5;
 const MAX_PER_DAY = 2;
