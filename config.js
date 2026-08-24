2f69f05e1735f1f04b9cc1f5b397e5fd8d27b7ba
Шаферов Вячеслав Александрович <vashafero1@mts.ru>
2026-08-24 17:39:44 +0500

    upd

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const API_URL = 'https://script.google.com/macros/s/AKfycbyuqQnSmifsK9tqLJnXx5GcTrjCA_V6rEqSh95wVQuVWIcdrqieRs6r0KoQID1Bwcw/exec';
-const PAGE_SIZE = 5;
+const PAGE_SIZE = 4;
 const MAX_PER_DAY = 2;
-const REPERTOIRE_PAGE_SIZE = 5;
+const REPERTOIRE_PAGE_SIZE = 4;
+const SONGS_PAGE_SIZE = 5;
